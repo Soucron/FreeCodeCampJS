3e3e70139434ca079d6270c958c61599f8474522
Iniok <ilidanilo@mail.ru>
2022-12-07 21:24:42 +0300

    Basic Algorithm

---
+++ Reverse a String.js
@@ -0,0 +1,10 @@
+
+//Reverse the provided string and return the reversed string.
+//For example, "hello" should become "olleh".
+
+
+Solution: 
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+  }
